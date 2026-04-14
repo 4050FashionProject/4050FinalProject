55d0acd756452191e3833f7907ce59f09190502a
akiuga <ajj54543@uga.edu>
2026-04-14 01:17:11 -0400

    feat: login and register pages

---
+++ frontend/src/LoginPage.jsx
@@ -0,0 +1,25 @@
+import "./styles/LoginPage.css";
+import { useNavigate } from "react-router-dom";
+
+function LoginPage() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <form action="">
+        <h2>Log In</h2>
+        <input placeholder="username" type="text" />
+        <input placeholder="password" type="text" />
+        <button className="primary-button">Log In</button>
+        <a href="">forgot password?</a>
+        <button
+          className="secondary-button"
+          onClick={() => navigate("/register")}
+        >
+          Create new account
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default LoginPage;
---
+++ frontend/src/RegisterPage.jsx
@@ -0,0 +1,24 @@
+import "./styles/RegisterPage.css";
+import { useNavigate } from "react-router-dom";
+
+function RegisterPage() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <form action="">
+        <h2>Sign Up</h2>
+        <input placeholder="full name" type="text" />
+        <input placeholder="username" type="text" />
+        <input placeholder="password" type="password" />
+        <button className="primary-button">Register</button>
+        <a href="">already have an account?</a>
+        <button className="secondary-button" onClick={() => navigate("/login")}>
+          Log In
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default RegisterPage;
